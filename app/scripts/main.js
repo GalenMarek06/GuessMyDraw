0efa5169c5f0eb36c65ae95f176ce8ec997a489c
Romain <cabane.romain@gmail.Com>
2018-02-12 16:43:07 +0100

    nouvelle interface2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,40 @@
 $( document ).ready(function() 
 {
     $("#btn_GenerateWord").on("click",generateWord);
+    $("#btn_findGame").on("click",accessPlayersSalon);
+
+    $("#playersRoomScreen").on("click",accessGameScreen);
 
     function generateWord()
     {
         console.log("click");
        $('.card').css('transform', 'rotateY(180deg)');
+       myVar = setTimeout(accessDrawWordScreen, 4000);
     }
+
+    function accessPlayersSalon()
+    {
+        $("#mainScreen").hide();
+        $("#playersRoomScreen").show();
+    }
+
+    function accessGameScreen()
+    {
+        $("#playersRoomScreen").hide();
+        $("#gameScreen").show();
+        
+        $("#findWordScreen").show();
+        $("#drawWordScreen").hide();
+        $("#guessWordScreen").hide();
+    }
+
+    function accessDrawWordScreen()
+    {
+         $("#findWordScreen").hide();
+         $("#drawWordScreen").show();
+    }
+
+    $("#playersRoomScreen").hide();
+    $("#gameScreen").hide();
 
 });
