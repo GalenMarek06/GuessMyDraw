5c9ef6d4c4f200e01d9f9f66b68bd9ff710f4832
antonyveyre <veyre@icloud.com>
2018-02-12 22:54:28 +0100

    inscription

--- app.js
+++ app.js
@@ -4,27 +4,47 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 let sequenceNumberByClient = new Map();
+let listOfUsers = []
+var status = {
+  CONNECTED : {value: 1, name: "Connecté"},
+  INGAME: {value: 2, name: "Joue"},
+};
+
 
 app.use(express.static(__dirname + '/app'));
 app.get('/', function(req, res,next) {
-    res.sendFile(__dirname + '/paint.html');
+  res.sendFile(__dirname + '/paint.html');
 });
 
+class User {
+  constructor(id) {
+    this.id=id;
+    this.status=status.CONNECTED;
+  }
 
+}
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  listOfUsers.push(new User(socket.id))
   console.info(`Client connected [id=${socket.id}]`);
   sequenceNumberByClient.set(socket, 1);
-
+  console.log(listOfUsers);
   socket.on('canvas', function(msg){
-    console.log('canvas sending');
     io.emit('canvas', msg);
   });
+
+  socket.on('inscription',function(msg){
+    console.log('inscription');
+    let obj = listOfUsers.find(o => o.id === socket.id);
+    obj.name = msg;
+    console.log(obj);
+    io.emit('inscription',obj);
+  })
   socket.on("disconnect", () => {
-          sequenceNumberByClient.delete(socket);
-          console.info(`Client gone [id=${socket.id}]`);
-      });
+    sequenceNumberByClient.delete(socket);
+    console.info(`Client gone [id=${socket.id}]`);
+  });
 });
 
 server.listen(4200, function(){
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+var socket = io();
+
 
 
 $("#btn_GenerateWord").on("click",generateWord);
@@ -9,3 +11,34 @@ function generateWord()
   console.log("click");
   $('.card').css('transform', 'rotateY(360deg)');
 }
+
+
+function addUser(User){
+  let div = document.createElement('div')
+  div.className = 'playerGame';
+  let labName = document.createElement('label');
+  labName.className = 'playerName';
+  labName.textContent = User.name;
+  let labStatus = document.createElement('label');
+  labStatus.className = 'playerConnection';
+  labStatus.textContent = User.status.name;
+  div.id = User.id;
+  div.append(labName);
+  div.append(labStatus);
+  $('.guessMyDrawContainer').append(div);
+
+}
+
+$('#playerInfo').submit(function(e){
+  e.preventDefault();
+  console.log($('#playerName').val());
+  socket.emit('inscription',$('#playerName').val());
+  return;
+
+} )
+;
+
+socket.on('inscription',function(data){
+  console.log(data);
+  addUser(data);
+})
