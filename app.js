313ec44a93b2445c769fa9e8c01fa0c21867796f
antonyveyre <veyre@icloud.com>
2018-02-12 20:34:28 +0100

    drawing correction

--- app.js
+++ app.js
@@ -2,19 +2,28 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var socket = require('socket.io')(server);
+let sequenceNumberByClient = new Map();
 
 app.use(express.static(__dirname + '/app'));
 app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/paint.html');
 });
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('canvas', function(msg){
-    console.log('message: ' + msg);
-    io.emit('canvas', msg);
-  });
 
+
+
+socket.on('connection', function(socket){
+  console.log('a user connected');
+  console.info(`Client connected [id=${socket.id}]`);
+  sequenceNumberByClient.set(socket, 1);
+
+  socket.on('canvas', function(msg){
+    socket.emit('canvas', msg);
+  });
+  socket.on("disconnect", () => {
+          sequenceNumberByClient.delete(socket);
+          console.info(`Client gone [id=${socket.id}]`);
+      });
 });
 
 server.listen(4200, function(){
