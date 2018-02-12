82645bb1bf8d3ef0068b7b302965f1c244e709fc
antonyveyre <veyre@icloud.com>
2018-02-12 15:07:08 +0100

    word generator added on /word path

--- server.js
+++ server.js
@@ -10,9 +10,21 @@ app.use(
 );
 
 
+app.get('/word', function (req, res) {
+	let fs = require("fs");
+	var text = fs.readFileSync("./dico.txt").toString('utf-8');
+	var textByLine = text.split("\n")
+	let rand = Math.floor(Math.random() * (textByLine.length - 1));
+	res.send(textByLine[rand]);
+
+
+
+	//  res.send('Hello World!');
+});
+
 app.listen(PORT);
 
 console.log(`-----------------------------
-| The root folder is: '${__dirname}/app'
-| You can access the application at: http://localhost:${PORT}
-------------------------------------------`);
+	| The root folder is: '${__dirname}/app'
+	| You can access the application at: http://localhost:${PORT}
+	------------------------------------------`);
